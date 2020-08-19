d24819d82ad3bae85db338efbec0b92086cd9be4
Jonathan Cantor <jonatcantor@gmail.com>
2020-08-19 02:39:04 -0400

    updated playability

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,11 +7,6 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
-    //Velocidad con la que se movera tanto el jugador como la ia
-    //tienen la misma velocidad porque siempre estarán de frente
-    [Range(1, 5)]
-    public float speed = 1f;
-
     //Radio del circulo donde caminarán y donde se crearan los collectables
     [Range(1, 5)]
     public int radius = 1;
@@ -23,14 +18,6 @@ public class GameManager : MonoBehaviour
     //Los items que existirán en el juego
     public GameObject[] collectables;
 
-    //Angulo del juego, es el encargado de realizar el movimiento del jugador y la ia
-    [HideInInspector]
-    public float angle = 0f;
-
-    //Direccion en la que se gira el movimiento del juego
-    [HideInInspector]
-    public float direction = 0f;
-
     //Index del lugar donde se puso el ultimo collectable (el que se ve en pantalla)
     [HideInInspector]
     public int lastItemIndex = 0;
@@ -40,10 +27,18 @@ public class GameManager : MonoBehaviour
     public int beforeItemIndex = 0;
 
     //Referencia al player
-    Transform player;
+    [HideInInspector]
+    public Transform player;
 
     //Referencia a la ia
-    Transform ia;
+    [HideInInspector]
+    public Transform ia;
+
+    [HideInInspector]
+    public bool isPlaying;
+
+    [HideInInspector]
+    public Transform currentCollectable;
 
     void Awake(){
         if(!instance){
@@ -55,62 +50,37 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    void Start(){
+    IEnumerator Start(){
         player = GameObject.Find("Player").transform;
         ia = GameObject.Find("IA").transform;
 
-        //Se halla los grados a los que estarán separados los lugares donde se colcoarán los collectables
-        //dependiendo de la cantidad que se quieran
-        float degrees = 360 / amountOfPoints;
+        while(true){
+            if(!isPlaying && !currentCollectable){
+                yield return new WaitForSeconds(0.5f);
+                InstantiateCollectable();
+            }
 
-        for(int i=0; i<amountOfPoints; i++){
-            //Se halla la posición del lugar
-            Vector3 pos = new Vector3(Mathf.Cos(i * degrees * Mathf.Deg2Rad), 0.5f, Mathf.Sin(i * degrees * Mathf.Deg2Rad)) * radius;
-
-            //Se muestra las posiciones de los lugares para verlos mejor
-            Debug.DrawRay(transform.position, (pos - transform.position).normalized * (pos - transform.position).magnitude, Color.yellow, Mathf.Infinity);
-
-            //Se crea un objeto vació
-            Transform temp = new GameObject().transform;
-
-            //Con la posición hallada antes
-            temp.position = pos;
-
-            //Y lo hacemos hijo del GameManager
-            temp.SetParent(transform);
-        }
-
-        //Se crea el primer collectable del mapa al inicio
-        InstantiateCollectable();
-    }
-
-    void Update(){
-        //Va actualizando el angulo del mapa poco a poco para ir moviendo al jugador y la ia
-        angle += direction * speed * Time.deltaTime;
-
-        if(Input.GetKeyDown(KeyCode.Escape)){
-            Application.Quit();
-        }
-
-        if(Input.GetKeyDown(KeyCode.Space)){
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            yield return null;
         }
     }
 
     //Crea un nuevo collectable en el mapa
     public void InstantiateCollectable(){
-        //Antes de crear uno nuevo se guarda el index del actual
-        beforeItemIndex = lastItemIndex;
+        int randomX = 0;
 
         do{
-            //Se elige un nuevo lugar aleatoreo donde poner el collectable
-            lastItemIndex = Random.Range(0, transform.childCount);
+            randomX = Random.Range(-1, 2);
+        }while(randomX == 0);
 
-            //Mientras que la distancia de este lugar no esté tan cerca de la posición del jugador ni de la ia
-            //Ya que se podría justo en es posición y no se veria cuando los agarra
-        }while(Vector3.Distance(transform.GetChild(lastItemIndex).position, player.position) < 1f || (Vector3.Distance(transform.GetChild(lastItemIndex).position, ia.position) < 1f));
-
-        //Finalmente se crea el collectable aleatoreo en la mapa 
-        Instantiate(collectables[Random.Range(0, collectables.Length)], transform.GetChild(lastItemIndex).position, Quaternion.identity);
+        currentCollectable = Instantiate(collectables[Random.Range(0, collectables.Length)], new Vector3(randomX, 0.6f, 0f), Quaternion.identity).transform;
     }
+
+    public void PlayerAttackToIA(){
+
+    }
+
+    public void IAAttackToPlayer(){
+
+    }
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,42 +12,49 @@ public class Player : MonoBehaviour
     //Daño que hará el jugador a la ia
     public int damage = 5;
 
+    //Velocidad con la que se moverá el jugador
+    public float speed = 20f;
+
     Rigidbody rig;
     Animator anim;
-    
-    //Referencia a la ia
-    Transform ia;
 
     //Nos dice si presionó las teclas y las volvió a soltar
     //para comprar que solo se mueva cada vez que presione una tecla
     //y no cuando las deja presionadas
     bool keyDown;
 
+    bool upCollectable;
+
+    bool fail;
+
+    [HideInInspector]
+    public float inputDirection = 0;
+
+    Vector3 initPosition;
+
     void Awake(){
         rig = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+    }
 
-        ia = GameObject.Find("IA").transform;
+    void Start(){
+        initPosition = transform.position;
     }
 
     void Update(){
-        //Si no se están moviento y la ia no se ha destruido
-        if(GameManager.instance.direction == 0 && ia){
-            //Y no tiene la tecla presionada
-            if(!keyDown){
-                //Entonces cambia la dirección del juego a la dirección que seleccionó
-                //con las teclas
-                GameManager.instance.direction = Input.GetAxisRaw("Horizontal");
+        //Si no se ha presionado la tecla, no se está jugando y presiona una tecla
+        if(!keyDown && !GameManager.instance.isPlaying && Input.GetAxisRaw("Horizontal") != 0 && GameManager.instance.currentCollectable){
+            //Y hacemos keyDown a true para que deje de comprobar por si se mantiene
+            //las teclas presionadas
+            keyDown = true;
 
-                //Y hacemos keyDown a true para que deje de comprobar por si se mantiene
-                //las teclas presionadas
-                keyDown = true;
-            }
+            //Entonces juega el jugador
+            Play();
         }
 
         //Si la ia no ha muerto entonces que se le quede mirando de frente
-        if(ia){
-            transform.LookAt(ia.position);
+        if(GameManager.instance.ia){
+            transform.LookAt(GameManager.instance.ia.position);
         }
 
         //Hasta que suelte las teclas
@@ -56,41 +63,75 @@ public class Player : MonoBehaviour
             //comprobar ahora si, si el jugador logra reaccionar antes que la ia)
             keyDown = false;
         }
+
+        //Si se está jugando se movera el jugador si presionó una tecla
+        if(GameManager.instance.isPlaying && inputDirection != 0){
+            float initDistance = 0f;
+
+            if(fail){
+                initDistance = Vector3.Distance(new Vector3(
+                    -GameManager.instance.currentCollectable.position.x,
+                    GameManager.instance.currentCollectable.position.y,
+                    GameManager.instance.currentCollectable.position.z
+                ), transform.position);
+
+                if(initDistance <= 0.01f){
+                    inputDirection = -inputDirection;
+                    upCollectable = true;
+                    fail = false;
+                }
+            }
+            
+            else if(!upCollectable && GameManager.instance.currentCollectable){
+                initDistance = Vector3.Distance(GameManager.instance.currentCollectable.position, transform.position);
+                
+                if(initDistance <= 0.01f){
+                    inputDirection = -inputDirection;
+                    upCollectable = true;
+                    //Ataca al jugador
+                    Attack();
+                    
+                    //Destruye el collectable
+                    Destroy(GameManager.instance.currentCollectable.gameObject);
+                }
+            }
+
+            else{
+                initDistance = Vector3.Distance(initPosition, transform.position);
+            }
+
+            transform.RotateAround(Vector3.zero, Vector3.up, -inputDirection * speed * initDistance * Time.deltaTime);
+
+            if(initDistance <= 0.001f && upCollectable){
+                upCollectable = false;
+                transform.position = initPosition;
+                GameManager.instance.isPlaying = false;
+                inputDirection = 0;
+            }
+        }
     }
 
-    void FixedUpdate(){
-        //Se mueve el jugador a la dirección que el diga
-        rig.MovePosition(new Vector3(Mathf.Cos(GameManager.instance.angle), 0f, Mathf.Sin(GameManager.instance.angle)) * GameManager.instance.radius);
+    void Play(){
+        inputDirection = Input.GetAxisRaw("Horizontal");
+
+        if(inputDirection != GameManager.instance.currentCollectable.position.x){
+            fail = true;
+        }
+
+        GameManager.instance.isPlaying = true;
     }
 
     //Ataque del jugador
     void Attack(){
         anim.SetTrigger("Attack");
-        //Debug.DrawRay(transform.position, (ia.position - transform.position).normalized * GameManager.instance.radius * 2, Color.yellow);
 
-        //Resta vida a la ia dependiendo del daño que haga
-        ia.GetComponent<IA>().health -= damage;
-        
-        //Si la vida de la ia es cero se destruye
-        if(ia.GetComponent<IA>().health <= 0){
-            Destroy(ia.transform.gameObject);
-        }
+        GameManager.instance.PlayerAttackToIA();
     }
 
     //Si el jugador choca con un collectable
     void OnTriggerEnter(Collider other){
         if(other.name != "IA"){
-            //Detiene el movimiento del juego
-            GameManager.instance.direction = 0f;
-
-            //Destryude el collectable
-            Destroy(other.gameObject);
-
-            //Ataca a la ia
-            Attack();
-
-            //Y crea un nuevo collectable en el mapa
-            GameManager.instance.InstantiateCollectable();
+            GameManager.instance.currentCollectable.GetComponent<Renderer>().enabled = false;
         }
     }
 }
