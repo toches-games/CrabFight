5ebf3873bcb0d86e2144c4d21afa7c5faa82e538
Jonathan Cantor <jonatcantor@gmail.com>
2020-08-20 05:03:43 -0400

    added camera animation

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    IEnumerator Start()
+    {
+        Vector3 targetPosition = transform.position;
+
+        while(true){
+            if(GameManager.instance.PVP || GameManager.instance.PVCOM){
+                while(Vector3.Distance(transform.position, targetPosition) > 0.001f){
+                    transform.RotateAround(Vector3.zero, Vector3.up, 100 * Vector3.Distance(transform.position, targetPosition) * Time.deltaTime);
+                    yield return null;
+                }
+
+                targetPosition = new Vector3(transform.position.x, transform.position.y, -transform.position.z);
+                yield return new WaitForSeconds(30f);
+            }
+
+            yield return null;
+        }
+    }
+}
